7634040c8dcf52867861b8e182d2830ea807950f
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-16 10:38:25 +0800

    4.02 add test button to listen to events & work with event handlers

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,10 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("This button was clicked!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +14,8 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      {/* <button onClick={() => {console.log('this button was clicked!')}}>Change Title</button> */}
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
