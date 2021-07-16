b9f1487df05775c4f5edd4a03c08865885a970fc
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-16 11:07:34 +0800

    4.03 add initial logic to ExpenseItem component to update the title on the page | try to make ExpenseItem dynamic - demonstrate how component functions differ in behavior to regular JS programming - code is incomplete because it doesn't handle State yet and does not correctly update the title on the page

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,15 +3,19 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  let title = props.title;
+
   const clickHandler = () => {
     console.log("This button was clicked!");
+    title = "Title updated!";
+    console.log(title);
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
       {/* <button onClick={() => {console.log('this button was clicked!')}}>Change Title</button> */}
