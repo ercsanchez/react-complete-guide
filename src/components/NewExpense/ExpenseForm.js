254656588a31d23d1e30681223bfcd0630a7bb74
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-17 10:12:40 +0800

    4.13 add two-way binding to be able to clear form inputs and set values on the input fields

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -70,6 +70,9 @@ const ExpenseForm = () => {
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
     console.log(expenseData);
   };
 
@@ -78,7 +81,11 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -86,6 +93,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -95,6 +103,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
