a31c4c39023e5e53ffc88a8bb1c99e15635ef91b
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-17 17:56:02 +0800

    5.09 add ExpensesChart to display Chart and use ExpensesChart in Expenses | complete code to compute maxValue of dataPoints to display correct fill of chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import { useState } from "react";
 
 const Expenses = (props) => {
@@ -22,6 +23,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onFilterChange={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
