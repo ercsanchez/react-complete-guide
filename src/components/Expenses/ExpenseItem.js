cc5f6b1875e4d2c28a5dcaa64070444ce01914ce
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-17 12:08:48 +0800

    4.16 clean up code | remove unneeded button and code in ExpenseItem.js

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,16 @@
 import "./ExpenseItem.css";
 
-import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Title Updated");
-    console.log("This button was clicked!");
-    console.log(title);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-      {/* <button onClick={() => {console.log('this button was clicked!')}}>Change Title</button> */}
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
