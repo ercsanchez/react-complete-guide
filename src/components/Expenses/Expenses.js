d7e311a4278a004a4cdfe94ca5fc5db0ea21d9a0
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-17 15:05:55 +0800

    5.05 output conditional content for ExpensesFilter - display a message if no expense item for the filtered year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,20 +15,54 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id} // you can also use index: 2nd param automatically passed to map
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onFilterChange={filterChangeHandler}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}  // you can also use index: 2nd param automatically passed to map
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+
+      {/* using ternary operator */}
+      {/* {filteredExpenses.length === 0 ? (
+        expensesContent
+      ) : (
+        filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id} // you can also use index: 2nd param automatically passed to map
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))
+      )} */}
+
+      {/* operator overloading */}
+      {/* {filteredExpenses.length === 0 && expensesContent }
+      {filteredExpenses.length > 0 &&
+        filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id} // you can also use index: 2nd param automatically passed to map
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))} */}
+
+      {/* best way */}
+      {expensesContent}
     </Card>
   );
 };
