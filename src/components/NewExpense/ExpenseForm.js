62b444572d9d97999fb1da892eca9ea0acf5f220
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-17 10:04:54 +0800

    4.12 handle form submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -63,8 +63,18 @@ const ExpenseForm = () => {
     // console.log(userInput.enteredDate);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
