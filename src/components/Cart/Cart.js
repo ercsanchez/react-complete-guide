cc413b48895f60269e2a5a565cf7931ffc6fbeb2
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-21 20:11:27 +0800

    11.15a output correct data on the cart and add validation - use context in Cart comp to display added meal items on shopping cart modal - remove hardcoded data in Cart comp - hide order button if no meal items currently added to the cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,26 +1,35 @@
+import { useContext } from "react";
+
 import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
+      {cartCtx.items.map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
   );
+
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>36.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.action}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
