dea69b53d9150d80c706eb352ee3f70dda49dd33
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-21 15:21:26 +0800

    11.12 consume cart-context in HeaderCartButton comp - show the correct order quantity in the cart for each menu item

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,15 @@
+import { useContext } from "react";
+
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce((currentValue, item) => {
+    return currentValue + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
@@ -10,7 +18,7 @@ const HeaderCartButton = (props) => {
       <span>Your Cart</span>
 
       {/* dummy value */}
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
