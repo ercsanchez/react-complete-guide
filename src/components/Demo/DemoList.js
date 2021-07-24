f5057ee98ec4a13daabb7f54eacea608e34c66e9
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-24 18:23:51 +0800

    12.11b use React.memo to prevent sortedList from running everytime DemoList is reexecuted

--- src/components/Demo/DemoList.js
+++ src/components/Demo/DemoList.js
@@ -4,6 +4,7 @@ import classes from "./DemoList.module.css";
 
 const DemoList = (props) => {
   const sortedList = props.items.sort((a, b) => a - b);
+  console.log("DemoList RUNNING");
 
   return (
     <div className={classes.list}>
@@ -17,4 +18,4 @@ const DemoList = (props) => {
   );
 };
 
-export default DemoList;
+export default React.memo(DemoList);
