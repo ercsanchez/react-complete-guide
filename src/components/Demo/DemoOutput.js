ee0cb21146c5438b80b143123dcff032527a3549
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-24 15:54:27 +0800

    12.04c reexecution of comps goes down the parent comp's comp. tree - all grandchildren of the parent comps will also be reexecuted when State changes

--- src/components/Demo/DemoOutput.js
+++ src/components/Demo/DemoOutput.js
@@ -1,6 +1,8 @@
+import MyParagraph from "./MyParagraph";
+
 const DemoOutput = (props) => {
   console.log("DemoOutput RUNNING");
-  return <p>{props.show ? "This is new!" : ""}</p>;
+  return <MyParagraph>{props.show ? "This is new!" : ""}</MyParagraph>;
 };
 
 export default DemoOutput;
