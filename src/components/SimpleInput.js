2f1b5fe53d7b8c9b16c5bb7a3d4d846422f8f4d1
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-26 10:55:55 +0800

    16.04 get user inputs and submit form using useState and useRef - in practice, only choose one option - choose based on what's needed:   - ref: you only need value once and using state is overkill   - state: need to monitor all changes of the value

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,38 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const nameInputRef = useRef();
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault(); // prevents default: send HTTP request to server for current page | current page reload and possible app restart | lose state
+
+    // state
+    console.log(enteredName);
+
+    // ref
+    const enteredValue = nameInputRef.current.value;
+    console.log("ref", enteredValue);
+
+    // nameInputRef.current.value = ""; // ref: not ideal since directly manipulating Real DOM instead of letting React handle that | allowed in special cases if you really just want to reset input
+    setEnteredName(""); // state: best practice for resetting input value since react handles DOM manipulation
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          value={enteredName}
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
