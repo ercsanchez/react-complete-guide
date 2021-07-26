ad09b94cd5a828d40c64d7adbb4b58de2c6f06ed
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-26 18:00:31 +0800

    17.06a add initial checkout form

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import Modal from "../UI/Modal";
 import CartItem from "./CartItem";
 import CartContext from "../../store/cart-context";
+import Checkout from "./Checkout";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
@@ -41,6 +42,7 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
+      <Checkout />
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
