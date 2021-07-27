529931a31c4c487cccdc12c38729a42ac283dfd8
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-27 10:48:04 +0800

    18.05b demo: redux concepts

---
+++ redux-demo.js
@@ -0,0 +1,25 @@
+const redux = require("redux");
+
+// reducer func - changes state based on action
+const counterReducer = (state = { counter: 0 }, action) => {
+  return { counter: state.counter + 1 };
+};
+
+//create store to hold state
+const store = redux.createStore(counterReducer); // tell redux which reducer func should be used to change state
+// reducer will be run when store is created
+
+// log initial state
+console.log("initial state:", store.getState());
+
+// subscriber - executes when state changes
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+// register subscriber func to redux / make redux aware of subscriber
+store.subscribe(counterSubscriber);
+
+// create and dispatch action
+store.dispatch({ type: "increment" }); // no effect other than to run reducer since no conditional in reducer to evaluate the action
