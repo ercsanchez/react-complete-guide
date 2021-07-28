1ee2a4925506c6b60b3295ced3193600feef6c3e
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-28 15:15:24 +0800

    19.11a option 2: use an action creator thunk for async code

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { useSelector, useDispatch } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { putCartData } from "./store/cart-slice";
 
 let isInitial = true;
 
@@ -16,46 +16,12 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const putCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      );
-      const response = await fetch(
-        "https://react-http-e9205-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success", // required since setting css classes using status
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    putCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error", // required since setting css classes using status
-          title: "Error!",
-          message: "Sending cart data failed!",
-        })
-      );
-    });
+    dispatch(putCartData(cart));
   }, [cart, dispatch]); // react-redux will ensure dispatch fn will not change but still add it to remove eslint warning
 
   return (
