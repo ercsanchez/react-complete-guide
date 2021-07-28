27bff5a625999e9618ce1ea54f455508006f6367
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-28 15:25:49 +0800

    19.12a separate putCartData in separate cart-actions file

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,7 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-import { uiActions } from "./ui-slice";
-
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
@@ -39,50 +37,6 @@ const cartSlice = createSlice({
     },
   },
 });
-
-//  create action creator thunk
-export const putCartData = (cart) => {
-  return async (dispatch) => {
-    dispatch(
-      uiActions.showNotification({
-        status: "pending",
-        title: "Sending...",
-        message: "Sending cart data!",
-      })
-    );
-
-    const putRequest = async () => {
-      const response = await fetch(
-        "https://react-http-e9205-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-    };
-
-    try {
-      await putRequest();
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success", // required since setting css classes using status
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    } catch (error) {
-      dispatch(
-        uiActions.showNotification({
-          status: "error", // required since setting css classes using status
-          title: "Error!",
-          message: "Sending cart data failed!",
-        })
-      );
-    }
-  };
-};
 
 export const cartActions = cartSlice.actions;
 
