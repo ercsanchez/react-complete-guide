0e8866484d0cf5ed0e60ffec85bbe12956a394d3
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-29 17:31:20 +0800

    20.23a write more flexible/dynamic urls using useRouteMatch | only use for nested routes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { useParams, Route, Link } from "react-router-dom";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
@@ -10,7 +10,10 @@ const DUMMY_QUOTES = [
 ];
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
+
+  console.log(match);
 
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
@@ -21,21 +24,20 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteId}`} exact>
+      <Route path={match.path} exact>
         <div className="centered">
-          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+          <Link className="btn--flat" to={`${match.url}/comments`}>
             Load Comments
           </Link>
         </div>
       </Route>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Route path={`${match.path}/comments`}>
         {/* Added this code on my own to provide hide functionality of comments */}
         <div className="centered">
-          <Link className="btn--flat" to={`/quotes/${params.quoteId}`}>
+          <Link className="btn--flat" to={match.url}>
             Hide Comments
           </Link>
         </div>
-        {/* alternative path if wrapped comp is not a link: path={'/quotes/:quoteId/comments'} */}
         <Comments />
       </Route>
     </Fragment>
