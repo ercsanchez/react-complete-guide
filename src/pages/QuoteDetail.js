e7154e8f19b0bd98005ed5d76e3a6159f57c3a58
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-29 15:50:02 +0800

    20.22 getting creative with nested routes | add a Link to Comments from QuoteDetail

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
@@ -21,7 +21,20 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
+        {/* Added this code on my own to provide hide functionality of comments */}
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}`}>
+            Hide Comments
+          </Link>
+        </div>
         {/* alternative path if wrapped comp is not a link: path={'/quotes/:quoteId/comments'} */}
         <Comments />
       </Route>
