3c111e5a368b8601d743d818450a820e04f92abe
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-29 11:51:53 +0800

    20.19 implement programmatic (imperative) navigation | navigate to AllQuotes page upon successful adding of new quote on QuoteForm - allow user to go back to previous page (NewQuote route)

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,9 +1,16 @@
+import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+    history.push("/quotes");
+    console.log(history);
   };
+
   return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 
