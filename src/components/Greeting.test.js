74d236684002dc5f2de59bd4e47deb8e300eb3d9
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-05 13:55:49 +0800

    26.08 group tests together with test suites

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("renders 'Hello World' as a text", () => {
-  // Arrange
-  render(<Greeting />);
+// test suite 1
+describe("Greeting component", () => {
+  // test 1
+  test("renders Hello World as a text", () => {
+    // Arrange
+    render(<Greeting />);
 
-  // Act
-  // ...nothing
+    // Act
+    // ...nothing
 
-  // Assert
-  const helloWorldElement = screen.getByText("Hello World!", { exact: true }); // default: true (no need to specify) | false matches substrings & case insensitive
-  expect(helloWorldElement).toBeInTheDocument();
+    // Assert
+    const helloWorldElement = screen.getByText("Hello World!", { exact: true }); // default: true (no need to specify) | false matches substrings & case insensitive
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });
