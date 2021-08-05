0d9c281bf8d41848ab6da0740fcf882d9be614b8
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-05 18:36:01 +0800

    26.11 test asynchronous code (GET request) - GET request to "https://jsonplaceholder.typicode.com/posts"

---
+++ src/components/Async.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import Async from "./Async";
+
+describe("Async component", () => {
+  test("renders posts if request succeeds", async () => {
+    render(<Async />);
+
+    const listItemElements = await screen.findAllByRole("listitem"); // 3rd args: timeout pd. | default: 1s
+    expect(listItemElements).not.toHaveLength(0);
+  });
+});
