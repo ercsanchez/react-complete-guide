7360505755a777d9292b6feb651fd3de76d43b35
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-05 14:16:09 +0800

    26.09a test user interaction & state

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 // test suite 1
@@ -14,5 +15,23 @@ describe("Greeting component", () => {
     // Assert
     const helloWorldElement = screen.getByText("Hello World!", { exact: true }); // default: true (no need to specify) | false matches substrings & case insensitive
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders 'good to see you' if the button was NOT clicked", () => {
+    render(<Greeting />);
+    const outputElement = screen.getByText("good to see you", { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("renders 'Changed!' if button was clicked", () => {
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.getByText("Changed!"); // no need to specify exact if using default true
+    expect(outputElement).toBeInTheDocument();
   });
 });
