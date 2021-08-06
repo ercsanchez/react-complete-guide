c9b56d998ba36086fb0787e9a49cdcb538d2ee6f
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-06 19:25:03 +0800

    27.18 refs & useref | NewTodo comp

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,12 +1,26 @@
+import { useRef } from "react";
+
 const NewTodo = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+    // ? if current can be undefined or string  ! if we are sure that current can only be string
+    // ide autocompletes current?.value since it doesn't know that its impossible for enteredText to be undefined
+    // ref connection (rendering of <input> with ref prop) is established first before this statement can be executed
+
+    if (enteredText.trim().length === 0) {
+      // throw an error
+      return;
+    }
   };
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">
-        <input type="text" id="text" />
+        <input type="text" id="text" ref={todoTextInputRef} />
         <button>Add Todo</button>
       </label>
     </form>
