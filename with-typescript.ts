306f56760c9d5b9885433e38d82c2334948694f0
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-06 09:14:20 +0800

    27.02b demo: typescript benefits

--- with-typescript.ts
+++ with-typescript.ts
@@ -1,8 +1,8 @@
-function add1(a, b) {
+function add2(a: number, b: number) {
   return a + b;
 }
 
 // const result = add(2, 5);
-const result = add("2", "5");
+const result = add2("2", "5");
 
 console.log(result);
