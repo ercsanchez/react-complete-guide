d689a5d13078888dbf8c7c3f7821e6639805df8b
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-06 19:49:08 +0800

    27.20 manage state | App comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
+
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const addTodoHandler = (todoText: string) => {
-    // manage todos array
+    const newTodo = new Todo(todoText);
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
   };
 
   return (
