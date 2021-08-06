7f2dee959a72307b3bc1fe78e6d2dfb93b0cc3b4
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-06 14:28:20 +0800

    27.10 generics | 27.11 closer look at generics

--- basics.js
+++ basics.js
@@ -1,6 +1,11 @@
 // Primitives: number, string, boolean
 // More complex types: arrays, objects
 // Function types, parameters
+var __spreadArray = (this && this.__spreadArray) || function (to, from) {
+    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
+        to[j] = from[i];
+    return to;
+};
 // Primitives
 var age;
 age = 12;
@@ -14,8 +19,8 @@ hobbies = ["Sports", "Cooking"];
 // object type
 var person;
 person = {
-  name: "Max",
-  age: 32,
+    name: "Max",
+    age: 32
 };
 // error
 // person = {
@@ -26,3 +31,38 @@ var people;
 // Type inference
 var course = "React - The Complete Guide";
 course = 12341;
+// Functions & types
+// return value type inference
+function add(a, b) {
+    return a + b;
+}
+// explicit return type
+// function add(a: number, b: number): number {
+//   return a + b;
+// }
+function printOutput(value) {
+    console.log(value);
+}
+// Generics
+//helper function
+function insertAtBeginning(array, value) {
+    // benefit of ts lost due to any value in array
+    var newArray = __spreadArray([value], array);
+    return newArray;
+}
+var demoArray = [1, 2, 3];
+// updatedArray infers any value in array
+var updatedArray = insertAtBeginning(demoArray, -1); // -> [-1, 1, 2, 3]
+// updatedArray[0].split(""); // ts wont show any warnings but this will produce a runtime error coz cant call split on numbers array
+// generic type: T | T is a name convention and placeholder | T could be any alphabetical char
+function genericInsertAtBeginning(array, value) {
+    var newArray = __spreadArray([value], array);
+    return newArray;
+}
+var updatedArray2 = genericInsertAtBeginning(demoArray, -1); // -> [-1, 1, 2, 3]
+var stringArray = genericInsertAtBeginning(["a", "b", "c"], "d");
+// updatedArray2[0].split(""); // ts shows error
+stringArray[0].split(""); // no error
+// sometimes ts cant infer the type
+var stringArray2 = genericInsertAtBeginning(["a", "b", "c"], "d");
+console.log(stringArray2);
--- basics.ts
+++ basics.ts
@@ -65,3 +65,39 @@ function add(a: number, b: number) {
 function printOutput(value: any) {
   console.log(value);
 }
+
+// Generics
+
+//helper function
+function insertAtBeginning(array: any[], value: any) {
+  // benefit of ts lost due to any value in array
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+// updatedArray infers any value in array
+const updatedArray = insertAtBeginning(demoArray, -1); // -> [-1, 1, 2, 3]
+
+updatedArray[0].split(""); // ts wont show any warnings but this will produce a runtime error coz cant call split on numbers array
+
+// generic type: T | T is a name convention and placeholder | T could be any alphabetical char
+function genericInsertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const updatedArray2 = genericInsertAtBeginning(demoArray, -1); // -> [-1, 1, 2, 3]
+
+const stringArray = genericInsertAtBeginning(["a", "b", "c"], "d");
+
+updatedArray2[0].split(""); // ts shows error
+stringArray[0].split(""); // no error
+
+// sometimes ts cant infer the type | explicit setting of type
+const stringArray2 = genericInsertAtBeginning<string>(["a", "b", "c"], "d");
+
+// these are the same
+let numbers: number[] = [1, 2, 3]; // syntactic sugar
+let numbers2: Array<number> = [1, 2, 3]; // Arrays are of type Array
