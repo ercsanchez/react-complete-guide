ca48a5ecabc26d98ae06c9cb517739d36afd7081
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-06 13:54:43 +0800

    27.07 union types: |

--- basics.js
+++ basics.js
@@ -1,51 +1,28 @@
 // Primitives: number, string, boolean
 // More complex types: arrays, objects
 // Function types, parameters
-
 // Primitives
-
-let age: number;
-
+var age;
 age = 12;
-
-let userName: string;
-
+var userName;
 userName = "Max";
-
-let isInstructor: boolean;
-
+var isInstructor;
 isInstructor = true;
-
 // More complex types
-
-let hobbies: string[];
-
+var hobbies;
 hobbies = ["Sports", "Cooking"];
-
 // object type
-let person: {
-  name: string;
-  age: number;
-};
-
+var person;
 person = {
-  name: "Max",
-  age: 32,
+    name: "Max",
+    age: 32
 };
-
 // error
 // person = {
 //   isEmployee: true
 // };
-
 // combining types: array of objects
-let people: {
-  name: string;
-  age: number;
-}[];
-
+var people;
 // Type inference
-
-let course: string | number = "React - The Complete Guide";
-
+var course = "React - The Complete Guide";
 course = 12341;
